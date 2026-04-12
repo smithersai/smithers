21b54e8f5a51691c2161ecfe7d211afa5fea303d
William Cory <willcory10@gmail.com>
2026-04-11 23:54:52 -0700

    🗑️ remove: docs generation scripts

--- scripts/generate-llms-txt.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bun
-import { writeFileSync } from "node:fs";
-import { generateLlmsFull } from "./docs-utils";
-
-const output = generateLlmsFull();
-
-writeFileSync("docs/llms-full.txt", output);
-console.log(`Generated docs/llms-full.txt (${output.length} chars, ~${Math.round(output.length / 4)} tokens)`);
